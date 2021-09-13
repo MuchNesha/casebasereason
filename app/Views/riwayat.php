8cf1b50394850bafaaff5f2c4cf4fcc92d05f3d9
Bawik Ardiyan Ramadhan <47960612+Ardiyan16@users.noreply.github.com>
2021-09-14 04:19:25 +0700

    Update riwayat.php

--- app/Views/riwayat.php
+++ app/Views/riwayat.php
@@ -44,7 +44,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Data milik <?= $k->nama ?></h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Data dari <?= $k->nama ?></h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
